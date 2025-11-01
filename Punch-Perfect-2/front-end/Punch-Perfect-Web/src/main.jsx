3249a09ac3bf94e3f9c84513b5cac691fb5545c0
Aaron Xu <aaronssupmail@gmail.com>
2025-10-31 22:10:47 -0400

    added about section and back button

--- Punch-Perfect-2/front-end/Punch-Perfect-Web/src/main.jsx
+++ Punch-Perfect-2/front-end/Punch-Perfect-Web/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Menu from './Menu.jsx'
 import App from './App.jsx'
+import About from './About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/game" element={<App />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
