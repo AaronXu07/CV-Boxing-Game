a280556a9f982cc5f2651ee56ec52517590579ee
Brain256 <brianyin256@gmail.com>
2025-12-20 16:41:03 -0500

    changed leaderboard to reflect backend

--- Punch-Perfect-2/frontend/src/components/Account/Account.jsx
+++ Punch-Perfect-2/frontend/src/components/Account/Account.jsx
@@ -4,12 +4,14 @@ import './account.css'
 import { useSound } from '../../hooks/useSound.js'
 import { supabase } from '../../lib/supabase.js'
 import { getCurrentSession } from '../../lib/authFunctions.js'
+import { BarLoader } from 'react-spinners'; 
 
 function Account() {
   const navigate = useNavigate();
   const { playButtonSound } = useSound();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState('');
+  const [isLoading, setIsLoading] = useState(true); 
 
   const [highScores, setHighScores] = useState([]);
 
@@ -17,14 +19,21 @@ function Account() {
     const session = await getCurrentSession();
     if(!session) return;
 
-    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/scores/highscores`, {
-      headers: {
-        Authorization: `Bearer ${session.access_token}`
-      }
-    });
+    try {
+      setIsLoading(true); 
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/scores/highscores`, {
+        headers: {
+          Authorization: `Bearer ${session.access_token}`
+        }
+      });
 
-    const data = await res.json();
-    setHighScores(data);
+      const data = await res.json();
+      setHighScores(data);
+    } catch (err) {
+      console.error("Error occured: ", err); 
+    } finally {
+      setIsLoading(false); 
+    }
   }
 
   // Check if user is logged in
@@ -68,52 +77,55 @@ function Account() {
     }
   };
 
-
   return (
     <div className="account-container">
       <button className="back-button" onClick={handleBack}> ← Back</button>
 
-      <h1>Account</h1>
+      {isLoading ? <div className="spinner"><BarLoader color="#ed0c2e" width={200} height={8}/></div>
+        : <>
+            <h1>Profile</h1>
 
-      <div className="account-content">
-        {isLoggedIn ? (
-          <>
-            <div className="user-info">
-              <h2>{username}</h2>
-              <button className="logout-button" onClick={handleLogout}>
-                Logout
-              </button>
-            </div>
+            <div className="account-content">
+              {isLoggedIn ? (
+                <>
+                  <div className="user-info">
+                    <h2>{username}</h2>
+                    <button className="logout-button" onClick={handleLogout}>
+                      Logout
+                    </button>
+                  </div>
 
-            {/* High Scores Section */}
-            <div className="high-scores-section">
-              <h2>Your High Scores</h2>
-              <div className="high-scores-grid">
-                {highScores.map((item, index) => (
-                  <div key={index} className="score-card">
-                    <div className="score-card-mode">{item.mode}</div>
-                    <div className="score-card-value">
-                      {item.highscore}{item.mode === 'Reaction' ? ' ms' : ''}
+                  {/* High Scores Section */}
+                  <div className="high-scores-section">
+                    <h2>Your High Scores</h2>
+                    <div className="high-scores-grid">
+                      {highScores.map((item, index) => (
+                        <div key={index} className="score-card">
+                          <div className="score-card-mode">{item.mode}</div>
+                          <div className="score-card-value">
+                            {!item.highscore ? '-' : item.highscore }
+                            {item.mode === 'Reaction' && item.highscore ? ' ms' : ''}
+                          </div>
+                        </div>
+                      ))}
                     </div>
                   </div>
-                ))}
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="not-logged-in">
-              <div className="login-prompt">
-                <h2>Sign in to track your progress</h2>
-                <p>Create an account or log in to save your high scores and compete on the leaderboard.</p>
-                <button className="login-button" onClick={handleLogin}>
-                  Login / Sign Up
-                </button>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
+                </>
+              ) : (
+                <>
+                  <div className="not-logged-in">
+                    <div className="login-prompt">
+                      <h2>Sign in to track your progress</h2>
+                      <p>Create an account or log in to save your high scores and compete on the leaderboard.</p>
+                      <button className="login-button" onClick={handleLogin}>
+                        Login / Sign Up
+                      </button>
+                    </div>
+                  </div>
+                </>
+              )}
+            </div> 
+          </>}
     </div>
   );
 }
