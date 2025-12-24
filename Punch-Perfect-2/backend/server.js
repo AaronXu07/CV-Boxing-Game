acf56ab7c3de7d1c3827ef528d7753b2167628af
AaronXu07 <121455537+AaronXu07@users.noreply.github.com>
2025-12-23 19:32:40 -0500

    added rate limiting

--- Punch-Perfect-2/backend/server.js
+++ Punch-Perfect-2/backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config(); 
 
 import cors from "cors"; 
+import rateLimit from "express-rate-limit";
 
 import scoreRoutes from "./routes/scores_routes.js"; 
 import leaderboardRoutes from "./routes/leaderboard_routes.js"; 
@@ -12,6 +13,18 @@ const app = express();
 
 app.use(cors()); 
 app.use(express.json()); 
+
+// Rate Limiter Configuration
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 250, // Limit each IP to 100 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: { error: "Too many requests from this IP, please try again later." }
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 const PORT = process.env.PORT; 
 
